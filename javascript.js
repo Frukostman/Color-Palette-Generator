f20ed2a50d7ebab3a0d0e8fc4a2a5cf69e1cc126
Santiago Homps <santiagohomps@gmail.com>
2021-05-24 10:29:57 -0300

    nombre

---
+++ javascript.js
@@ -0,0 +1,77 @@
+//Rectangulos color
+
+const R1 = document.querySelector("#color1");
+const R2 = document.querySelector("#color2");
+const R3 = document.querySelector("#color3");
+const R4 = document.querySelector("#color4");
+const R5 = document.querySelector("#color5");
+
+//Textos con codigos
+
+const texto1 = document.querySelector("#textoColor1")
+const texto2 = document.querySelector("#textoColor2")
+const texto3 = document.querySelector("#textoColor3")
+const texto4 = document.querySelector("#textoColor4")
+const texto5 = document.querySelector("#textoColor5")
+
+// document.addEventListener('DOMContentLoaded', darColor)
+
+function darColor() {
+
+    let color1A = Math.floor(Math.random()*256)
+    let color1B = Math.floor(Math.random()*256)
+    let color1C = Math.floor(Math.random()*256)
+    console.log(color1A)
+    console.log(color1B)
+    console.log(color1C)
+    let color2A = Math.floor(Math.random()*256)
+    let color2B = Math.floor(Math.random()*256)
+    let color2C = Math.floor(Math.random()*256)
+    console.log(color2A)
+    console.log(color2B)
+    console.log(color2C)
+    let color3A = Math.floor(Math.random()*256)
+    let color3B = Math.floor(Math.random()*256)
+    let color3C = Math.floor(Math.random()*256)
+    console.log(color3A)
+    console.log(color3B)
+    console.log(color3C)
+    let color4A = Math.floor(Math.random()*256)
+    let color4B = Math.floor(Math.random()*256)
+    let color4C = Math.floor(Math.random()*256)
+    console.log(color4A)
+    console.log(color4B)
+    console.log(color4C)
+    let color5A = Math.floor(Math.random()*256)
+    let color5B = Math.floor(Math.random()*256)
+    let color5C = Math.floor(Math.random()*256)
+    console.log(color5A)
+    console.log(color5B)
+    console.log(color5C)
+    
+    
+    R1.style = `background-color: rgb(${color1A}, ${color1B}, ${color1C});`;
+    texto1.innerHTML = `<p>RGB (${color1A}, ${color1B}, ${color1C});</p> `;
+    texto1.style = `color: white; opacity: 0.9; font-size: small;`
+    
+    R2.style = `background-color: rgb(${color2A}, ${color2B}, ${color2C});`;
+    texto2.innerHTML = `<p>RGB (${color2A}, ${color2B}, ${color2C});</p> `;
+    texto2.style = `color: white; opacity: 0.9; font-size: small;`
+
+    R3.style = `background-color: rgb(${color3A}, ${color3B}, ${color3C});`;
+    texto3.innerHTML = `<p>RGB (${color3A}, ${color3B}, ${color3C});</p> `;
+    texto3.style = `color: white; opacity: 0.9; font-size: small;`
+
+    R4.style = `background-color: rgb(${color4A}, ${color4B}, ${color4C});`;
+    texto4.innerHTML = `<p>RGB (${color4A}, ${color4B}, ${color4C});</p> `;
+    texto4.style = `color: white; opacity: 0.9; font-size: small;`
+
+    R5.style = `background-color: rgb(${color5A}, ${color5B}, ${color5C});`;
+    texto5.innerHTML = `<p>RGB (${color5A}, ${color5B}, ${color5C});</p> `;
+    texto5.style = `color: white; opacity: 0.9; font-size: small;`
+
+
+  
+}
+
+// document.addEventListener(onclick, darColor())
